8f60e674e9846736cba0a8210cb4cfc4982e6f58
Denis <denis.saz@gmail.com>
2017-02-20 22:07:18 -0500

    Email feature. Remove ads

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from bs4 import BeautifulSoup
-import urllib2
+import smtplib
 import requests
 from collections import OrderedDict
 import sqlite3
+import ConfigParser
+from email import message
 
 url = "http://www.kijiji.ca"
 city = "Toronto+%28GTA%29"
 db = "auto"
-searchTerm = "hyundai genesis"
+searchTerm = "car"
 locationId = "1700272"  # Needs this one, city seems irrelevant
 minprice = ""
 maxprice = ""
@@ -61,14 +63,29 @@ def soupifyRawResult(searchResult):
     # print infoDiv[0]
 
 
+def initConfig():
+    global mail_user
+    global mail_password
+    global db_location
+    global mail_receiver
+
+    config = ConfigParser.ConfigParser()
+    config.read("/home/d/Documents/PythonProjects/KijijiScraper/app.conf")
+    db_location = config.get("DB_Settings", "db_location")
+    mail_user = config.get("Mail_Settings", "username")
+    mail_password = config.get("Mail_Settings", "password")
+    mail_receiver = config.get("Mail_Settings", "receiver")
+
+
 def initDB():
     global conn
     global c
+
     sql_query = """CREATE TABLE if not exists {0}
         (id INTEGER PRIMARY KEY, post_title text,post_desc text,
         post_address text, post_price text, post_date text,
         post_url text)""".format(db)
-    conn = sqlite3.connect("/home/d/Documents/PythonProjects/kijiji.db")
+    conn = sqlite3.connect(db_location)
     conn.text_factory = lambda x: unicode(x, "utf-8", "ignore")
     c = conn.cursor()
     c.execute(sql_query)
@@ -77,21 +94,27 @@ def initDB():
 
 def insertToDB(postList):
     counter = 0
-    for post in postList:
+    postList_clone = postList[:]
+    print "List size: " + str(len(postList))
+    for post in postList_clone:
+        # print "Working with " + str(post)
         c = conn.cursor()
         c.execute(
             "SELECT * from " + db + " where post_url=?", (post[5],))
         if(c.fetchone()):
             # print "Skipping {0} Already exists".format(post[5])
-            pass
+            postList.remove(post)
+            # print "List length is now " + str(len(postList))
         else:
             c.execute("""insert INTO """ + db + """ (post_title, post_desc, post_address,
             post_price, post_date, post_url)
             VALUES (?,?,?,?,?,?);""", (post[0], post[1], post[2], post[3],
                                        post[4], post[5]))
             counter = counter + 1
-    conn.commit()
+    # conn.commit()
+    print "List size is now: " + str(len(postList))
     print "Done! Inserted {0} entries".format(counter)
+    return postList
 
 
 def extractUsefulData(data):
@@ -120,17 +143,74 @@ def extractUsefulData(data):
         post_price = post.find("div", class_="price").contents[
             0].encode("utf-8").strip()
         # postList.append([post_date])
-        postList.append([post_title, post_description, post_address,
-                         post_price, post_date, post_url])
+        if ("topAdSearch" not in post_url):  # Don't show ads
+            postList.append([post_title, post_description, post_address,
+                             post_price, post_date, post_url])
     return postList
 
 
+def formEmail(postList):
+    body = ""
+    msg = message.Message()
+
+    for post in postList:
+        body += """
+            Title: {0}
+            Price: {1}
+            Description: {2}
+            Address: {3}
+            Link: www.kijiji.ca{4}
+
+            """.format(post[0], post[3], post[1], post[2], post[5])
+
+    subject = "Kijiji Scraper: {0} Matching Posting(s) for {1}".format(
+        str(len(postList)), searchTerm)
+    # email_text = """
+    #    From: {0}
+    #    To: {1}
+    #    Subject: {2}
+    #
+    #    {3}
+    #    """.format(mail_user, mail_user, subject, body)
+    msg.add_header("from", mail_user)
+    msg.add_header("to", mail_receiver)
+    msg.add_header("subject", subject)
+    msg.set_payload(body)
+    return msg.as_string()
+
+
+def sendEmail(postList):
+    email_text = formEmail(postList)
+
+    print "Formed email: "
+    print email_text
+    try:
+        server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+        server.ehlo()
+        server.login(mail_user, mail_password)
+        server.sendmail(mail_user, mail_user, email_text)
+        server.close()
+        print "Email sent!"
+        return 0
+    except Exception as e:
+        print "Something went wrong... " + str(e)
+        return 1
+
+
 def main():
+    initConfig()
     initDB()
     searchResult = fetchPage()
     soup = soupifyRawResult(searchResult)
     postList = extractUsefulData(soup)
-    insertToDB(postList)
+    postList_newEntries = insertToDB(postList)
+    if(len(postList_newEntries) > 0):
+        if (sendEmail(postList_newEntries) == 0):
+            conn.commit()
+        else:
+            print "Could not send mail, not committing newfound data"
+    else:
+        print "No new entries!"
 
 
 if __name__ == "__main__":
